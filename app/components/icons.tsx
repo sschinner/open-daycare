9d1e667f9957af894c95fc86830a74253805fba1
Sebastian <seba.schinner@gmail.com>
2026-09-21 14:08:09 -0300

    Agrego paso 3

--- app/components/icons.tsx
+++ app/components/icons.tsx
@@ -244,6 +244,24 @@ export function SearchIcon(props: IconProps) {
   );
 }
 
+export function ChevronDownIcon(props: IconProps) {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2.2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="m6 9 6 6 6-6" />
+    </svg>
+  );
+}
+
 export function ChevronRightIcon(props: IconProps) {
   return (
     <svg
